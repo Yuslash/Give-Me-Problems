9bf3830ce2b69bbc503c76d01f62c50a639a81cf
Yuslash <sibikrishna71@gmail.com>
2024-11-29 23:46:11 +0530

    fixex the typo error

---
+++ solving-arrays-problems/multiSum.js
@@ -0,0 +1,5 @@
+function multy(a, b) {
+  return a * b
+}
+
+module.exports = multy
--- solving-arrays-problems/sum.test.js
+++ solving-arrays-problems/sum.test.js
@@ -1,5 +1,10 @@
 const sum = require('./sum')
+const multy = require('./multiSum')
 
 test("add 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test("multiply 1 * 2 to be 3", () => {
+  expect(multy(1, 2)).toBe(2)
 })
