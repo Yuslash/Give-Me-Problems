99ac53da12d4483c26f47a20e79ac5a9b2f49c4c
Yuslash <sibikrishna71@gmail.com>
2024-11-30 05:00:38 +0530

    now i getting clarified

--- solving-arrays-problems/stock.js
+++ solving-arrays-problems/stock.js
@@ -1,9 +1,26 @@
 var stock = function(prices) {
 }
 
-min_price = //day of the indiceis ex 0 = 7
-  profit = // same as the indeices ex 0 = 7
-  max = // min_price - profit 
+min_prices = infinity
+max_profit = 0
+
+// we need two variables which is min_prices and max_profit
+
+// you can see first 
+if (prices[i] < min_prices) //if this true then
+{
+  min_prices = prices[i]
+}
+
+currentValue = prices[i] //now you cansee that prices i is also 7 right
+
+current_profit = currentValue - min_prices
+
+if (currentProfit > max_profit) {
+  max_profit = currentProfit
+}
+
+//but it should be in loop because its should have to check every indieces from the array
 
 var prices = [7, 1, 5, 3, 6, 4]
 
