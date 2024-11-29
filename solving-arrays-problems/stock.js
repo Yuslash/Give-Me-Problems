2443f1a76f75fe0c4bca7d0507bfd45fd6bfa870
Yuslash <sibikrishna71@gmail.com>
2024-11-30 03:54:34 +0530

    we have the concept now need to implement the function

--- solving-arrays-problems/stock.js
+++ solving-arrays-problems/stock.js
@@ -1,6 +1,10 @@
 var stock = function(prices) {
 }
 
+min_price = //day of the indiceis ex 0 = 7
+  profit = // same as the indeices ex 0 = 7
+  max = // min_price - profit 
+
 var prices = [7, 1, 5, 3, 6, 4]
 
 stock(prices)
