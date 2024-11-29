e4f1472f1e62159b24667600c34f2251039c4bbf
Yuslash <sibikrishna71@gmail.com>
2024-11-30 05:28:07 +0530

    Completed stokc

--- solving-arrays-problems/stock.js
+++ solving-arrays-problems/stock.js
@@ -19,6 +19,4 @@ var stock = function(prices) {
 
 }
 
-var prices = [7, 1, 5, 3, 6, 4]
-
-console.log(stock(prices))
+module.exports = stock
--- solving-arrays-problems/sum.test.js
+++ solving-arrays-problems/sum.test.js
@@ -1,5 +1,6 @@
 const sum = require('./sum')
 const multy = require('./multiSum')
+const stock = require('./stock')
 
 test("add 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3)
@@ -7,4 +8,9 @@ test("add 1 + 2 to equal 3", () => {
 
 test("multiply 1 * 2 to be 3", () => {
   expect(multy(1, 2)).toBe(2)
+})
+
+test("get the maximum profit from the prices", () => {
+  const prices = [7, 1, 5, 3, 6, 4]
+  expect(stock(prices)).toBe(5)
 })
