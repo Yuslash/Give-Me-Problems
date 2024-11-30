fc3e921bf38bd3ae55cbb232f0a7be43f5e919c5
Yuslash <sibikrishna71@gmail.com>
2024-12-01 03:32:05 +0530

    easy pesy

--- blind-75/containerDuplicate.js
+++ blind-75/containerDuplicate.js
@@ -1,5 +1,19 @@
 var containsDuplicate = function(nums) {
 
+  let seen = new Set()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (seen.has(nums[i])) {
+      return true
+    }
+
+    seen.add(nums[i])
+  }
+
+  return false
+
 }
 
 let nums = [1, 2, 3, 1]
+
+console.log(containsDuplicate(nums))
