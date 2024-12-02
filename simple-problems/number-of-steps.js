aef44ecb31af01f7444f227c54643d51458bc15f
Yuslash <sibikrishna71@gmail.com>
2024-12-02 05:31:01 +0530

    not good

--- simple-problems/number-of-steps.js
+++ simple-problems/number-of-steps.js
@@ -1,9 +1,28 @@
 var main = function(a, b) {
 
+  let steps = 0
+  let target = Math.min(...a)
+
+  for (let i = 0; i < a.length; i++) {
+    while (a[i] > target) {
+      if (a[i] >= b[i]) {
+        a[i] -= b[i]
+        steps++
+      } else {
+        return -1
+      }
+    }
+
+    if (a[i] < target) {
+      return -1
+    }
+  }
+
+  return steps
 
 }
 
-let a = []
-let b = []
+let a = [5, 7, 10, 5, 15]
+let b = [2, 2, 1, 3, 5]
 
 console.log(main(a, b))
