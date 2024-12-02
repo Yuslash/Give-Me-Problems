45dd0372949af79e995b3e50f668fb788c590fb1
Yuslash <sibikrishna71@gmail.com>
2024-12-03 03:08:57 +0530

    updated

--- blind-75/subarray.js
+++ blind-75/subarray.js
@@ -1,11 +1,10 @@
 var maximumSubarray = function(nums) {
 
   let cur_sum = 0 // we are initali the current sum to check whether the nums[i]  is less then cur_sum
+
   let max_sum = Number.MIN_SAFE_INTEGER
 
   for (let i = 0; i < nums.length; i++) {
-
-    let temp = cur_sum + nums[i] // we are adding this to get the maximum value
 
     if (cur_sum < nums[i]) { // initaily we are checking that if the value is less then nums[i] ex if -2 < -3 then yes so the cur_sum will be -2 
 
@@ -13,7 +12,7 @@ var maximumSubarray = function(nums) {
 
     } else {
 
-      cur_sum = temp // are the value is greater then the nums[i] means the temp value will be taken to update for cur_sum
+      cur_sum = cur_sum + nums[i] // if the cur_sum is not greater then cur_sum will have the two sum added cur_sum and nums[i]
     }
 
     if (cur_sum > max_sum) { // this condition will check whether the cur_sum is greater than max_sum if it is then it will update to the max_sum
