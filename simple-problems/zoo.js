c5763a3887a48638351e8cb6292a3061c3f1362d
Yuslash <sibikrishna71@gmail.com>
2024-12-02 23:29:43 +0530

    reduced the lines

--- simple-problems/zoo.js
+++ simple-problems/zoo.js
@@ -1,19 +1,14 @@
 let word = "zzoooo"
 
-let countz = 0
+let countz = 0, counto = 0
+
 for (let i = 0; i < word.length; i++) {
   if (word[i] == 'z') {
     countz++
-  }
-}
-
-let counto = 0
-for (let i = 0; i < word.length; i++) {
-  if (word[i] == "o") {
+  } else if (word[i] == 'o') {
     counto++
   }
 }
-
 if (counto == 2 * countz) {
   console.log('yes')
 } else {
