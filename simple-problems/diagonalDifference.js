60780e4df50d202e742ee087e05af138aa0cee69
Yuslash <sibikrishna71@gmail.com>
2024-12-03 17:56:57 +0530

    there is problem with sums of difference i think we should use Math.abs

--- simple-problems/diagonalDifference.js
+++ simple-problems/diagonalDifference.js
@@ -31,4 +31,40 @@ for (let i = 0; i < 3; i++) {
   }
 }
 
-console.log(squre)
+//console.log(squre)
+
+// so to calculate in both diagonal direction the main and secondary 
+// first main diagonal 
+// our size is 3 so the formal for to calulate diagonal is mainDiagonalCal += matrix[i][i] whcih is matrix[0][0]
+// the second iteration the matrix[i][i] will be matrix[1][1] and third iteration matrix[2][2]
+// with that you can calucalte with diagonal now secondary diagonal
+// secondaryDiagonal += is like matrix[size - 1 - i] so incase the size = 3 so matrix[3 - 1 - i] 
+// first iteration will be like matrix[i][ 3 - 1 - 0]  = 2 so we will get the last index of the array because the array start from 0
+// secod iteration matrix[1][3 - 1 -1 ] = 1 
+// third iteration matrix[2][3-2-1] = 0
+// i hope you may understand a bit 
+// lets get hands on code now
+
+var additionDigaonal = function(matrix) {
+
+  let mainDiagonal = 0
+  let secondDiagonal = 0
+  let difference = 0
+
+  for (let i = 0; i < matrix.length; i++) {
+    mainDiagonal += matrix[i][i]
+    secondDiagonal += matrix[i][matrix.length - 1 - i]
+  }
+
+  difference = mainDiagonal - secondDiagonal
+  return difference
+
+}
+
+let matrixs = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12]
+]
+
+console.log(additionDigaonal(matrixs))
