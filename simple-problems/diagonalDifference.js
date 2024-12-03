d899e40b62901ee5bee21debf3d13757163d5ad5
Yuslash <sibikrishna71@gmail.com>
2024-12-03 14:28:11 +0530

    updated

--- simple-problems/diagonalDifference.js
+++ simple-problems/diagonalDifference.js
@@ -1,15 +1,15 @@
 //first we need to how to create a squarematrix
 
 var diagonalDifference = function(size) {
+
   let matrix = []
 
   for (let i = 0; i < size; i++) {
-    matrix[i] = []
+    matrix[i] = [] // this will create a (size) number of rows inside the matrix 
     for (let j = 0; j < size; j++) {
-      matrix[i][j] = 0
+      matrix[i][j] = i + j // this will put all the value with num++ like 1,2,3 in a order 
     }
   }
-
 
   return matrix
 }
