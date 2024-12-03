a1f0d5a37a5403031f5c498bab89f980b199f6af
Yuslash <sibikrishna71@gmail.com>
2024-12-03 14:17:35 +0530

    created an square matrix but

--- simple-problems/diagonalDifference.js
+++ simple-problems/diagonalDifference.js
@@ -1,1 +1,19 @@
 //first we need to how to create a squarematrix
+
+var diagonalDifference = function(size) {
+  let matrix = []
+
+  for (let i = 0; i < size; i++) {
+    matrix[i] = []
+    for (let j = 0; j < size; j++) {
+      matrix[i][j] = 0
+    }
+  }
+
+
+  return matrix
+}
+
+let size = 3
+
+console.log(diagonalDifference(size))
