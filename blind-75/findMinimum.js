728fe26f41a935305ff582077d74e68b3ca36b9d
Yuslash <sibikrishna71@gmail.com>
2024-12-03 22:21:30 +0530

    still lot way to learn

--- blind-75/findMinimum.js
+++ blind-75/findMinimum.js
@@ -1,14 +1,42 @@
-var rotationArray = function(nums, k) {
+// we have to find the minimum number in the array using o log n which is binary search 
+//
+// so now how do we find the minimum number in simple way
+//
+//actually if the nums[mid] is less then the mid - 1 then return the nums[mid]
+//or if nums[mid]  is greater then mid + 1 then return the nums[mid]
 
-  for (let i = 0; i < k; i++) {
-    nums.unshift(nums.pop())
-    console.log(`steps ${i + 1}: ${nums}`)
+
+function findMin(arr) {
+
+  let leftPointer = 0
+  let rightPointer = arr.length - 1
+
+  while (leftPointer < rightPointer) {
+
+    let mid = Math.floor((leftPointer + rightPointer) / 2)
+
+    if (arr[mid] < arr[mid - 1]) {
+
+      return arr[mid]
+    }
+    else if (arr[mid] > arr[mid + 1]) {
+
+      return arr[mid + 1]
+    }
+    else if (arr[leftPointer] < arr[mid]) {
+
+      leftPointer = mid + 1
+
+    } else {
+
+      rightPointer = mid - 1
+
+    }
   }
+
+  return -1
 }
-let nums = [1, 2, 3, 4, 5, 6, 7]
-let k = 3
 
-rotationArray(nums, k)
+let arr = [4, 5, 6, 7, 0, 1, 2]
 
-const arr = ['sibi', 'krishna', 'EvaElfie']
-console.log(arr, arr.pop())
+console.log(findMin(arr))
---
+++ blind-75/sortArray.js
@@ -0,0 +1,14 @@
+var rotationArray = function(nums, k) {
+
+  for (let i = 0; i < k; i++) {
+    nums.unshift(nums.pop())
+    console.log(`steps ${i + 1}: ${nums}`)
+  }
+}
+let nums = [1, 2, 3, 4, 5, 6, 7]
+let k = 3
+
+rotationArray(nums, k)
+
+const arr = ['sibi', 'krishna', 'EvaElfie']
+console.log(arr, arr.pop())
