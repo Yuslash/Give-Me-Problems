955a2c868e5e858e035a857e12f01ca65b057b8b
Yuslash <sibikrishna71@gmail.com>
2024-12-04 22:58:11 +0530

    so far

---
+++ simple-problems/palindrom.js
@@ -0,0 +1,9 @@
+let num = 121
+
+// now what we have to do is convert the number to string how below see
+
+let sumString = num.toString()
+//now we should seperate those element by splitini with ""
+let splitString = sumString.split("")
+
+console.log(splitString)
