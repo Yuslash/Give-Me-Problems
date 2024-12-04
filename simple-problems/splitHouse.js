6656eff7dec14b2ef12769f73cba9cc59787ba55
Yuslash <sibikrishna71@gmail.com>
2024-12-04 19:33:15 +0530

    why

--- simple-problems/splitHouse.js
+++ simple-problems/splitHouse.js
@@ -1,1 +1,15 @@
 // how to replace house with (.)
+//
+
+let n = ['.', 'H', '.', '.', '.']
+
+for (let i = 0; i < n.length; i++) {
+  if (n[i] === 'H') {
+    if (i + 1 < n.length && n[i + 1] === '.') {
+      n[i + 1] = 'B'
+    }
+  }
+
+}
+
+console.log(n)
