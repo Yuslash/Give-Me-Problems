881e4b5f6496e8714c29b167bf362845b009c382
Yuslash <sibikrishna71@gmail.com>
2024-12-04 22:15:53 +0530

    i just wannabe part of symphonyyy yeahh

---
+++ simple-problems/splitHouseAnalysis.js
@@ -0,0 +1,13 @@
+let n = 5
+let village = "H...H"
+
+let gridArr = []
+for (let i = 0; i < village.length; i++) {
+  gridArr[i] = village[i]
+}
+
+let anything = true
+
+if (anything) {
+  console.log("how are you")
+}
