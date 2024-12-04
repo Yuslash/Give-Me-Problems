268c5f4fe3cb9460e4be350d380e9ffe0458cbb9
Yuslash <sibikrishna71@gmail.com>
2024-12-04 23:14:42 +0530

    thats it

--- simple-problems/palindrom.js
+++ simple-problems/palindrom.js
@@ -6,9 +6,9 @@ let sumString = num.toString()
 //now we should seperate those element by splitini with ""
 let splitString = sumString.split("")
 
-let reverseString = splitString.reverse()
+let reverseString = splitString.reverse() // this will reverse the order if 234 means then it will become 432
 
-let joinString = reverseString.join("")
+let joinString = reverseString.join("") // we need to join to compare 
 
 if (sumString === joinString) {
 
