10fc83303333c8c3153a720bfb07ee5fb2667b0a
Yuslash <sibikrishna71@gmail.com>
2024-12-05 12:49:56 +0530

    i will become better programmar

--- simple-problems/stairCase.js
+++ simple-problems/stairCase.js
@@ -1,1 +1,25 @@
 // how to staircase
+
+var stair = function(rows, columns) {
+
+
+  for (let i = 0; i < rows; i++) {
+
+    let line = ''
+
+    for (let j = 0; j < columns; j++) {
+
+      line += '#'
+
+    }
+
+    console.log(line)
+  }
+
+
+}
+
+let rows = 5
+let columns = 5
+
+stair(rows, columns)
