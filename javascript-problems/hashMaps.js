53a8dd6fefca0a02af5ca0ca11d21f7692da8f89
Yuslash <sibikrishna71@gmail.com>
2024-12-05 17:45:53 +0530

    nothing just an inchident

---
+++ javascript-problems/hashMaps.js
@@ -0,0 +1,12 @@
+let hashMap = {}
+
+hashMap['V'] = 5
+hashMap['X'] = 5
+hashMap['I'] = 5
+hashMap['L'] = 5
+hashMap['C'] = 5
+
+let keys = Object.keys(hashMap)
+
+console.log(keys[0])
+console.log(hashMap[keys[0]])
