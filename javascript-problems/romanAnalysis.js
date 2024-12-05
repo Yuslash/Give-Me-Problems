9ed9874321ddfec469efeaa376b10252a7a17af5
Yuslash <sibikrishna71@gmail.com>
2024-12-05 19:05:46 +0530

    there is no time

--- javascript-problems/romanAnalysis.js
+++ javascript-problems/romanAnalysis.js
@@ -8,11 +8,24 @@ romans['C'] = 100
 romans['D'] = 500
 romans['M'] = 1000
 
+
 let keys = Object.keys(romans)
 
-for (let i = 0; i < keys.length; i++) {
+//for (let i = 0; i < keys.length; i++) {
 
-  console.log(romans[keys[i]])
+// console.log(romans[keys[i]])
+
+//}
+
+let value = 0
+
+for (let i = 0; i < 3; i++) {
+
+  // how to update the value
+  if (romans[i] === romans['I']) {
+    value = romans[keys[i]]
+  }
 
 }
 
+console.log(value)
