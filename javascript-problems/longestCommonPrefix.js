b94bd63cbc835e1552cffc4be887508c844c31bf
Yuslash <sibikrishna71@gmail.com>
2024-12-06 15:48:08 +0530

    neck paining

--- javascript-problems/longestCommonPrefix.js
+++ javascript-problems/longestCommonPrefix.js
@@ -1,6 +1,31 @@
 var main = function(str) {
-    
-  // it is very easy but for me very tough because im best programmar
+
+  let currentIndex = 0
+
+  for(let i = 0; i < 2 - 1 ; i++) {
+
+      if(str[i][currentIndex] === str[i+1][currentIndex]) {
+
+        console.log("yeah its equal")
+        currentIndex += 1
+    } else {
+      console.log(str[i])
+    }
+
+  }
 
 }
-let str = ["flower", "flow", "flight"]
+
+let str = ["flower", "flow"]
+
+main(str)
+//
+//var anotherMain = function(str) {
+//
+//  console.log(str[0][0])
+//
+//}
+//
+//let str = ["flower", "flow"]
+//
+//anotherMain(str)
