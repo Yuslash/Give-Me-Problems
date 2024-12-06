1d5c861b8193d723f62411cfd72a6b405658e8ae
Yuslash <sibikrishna71@gmail.com>
2024-12-06 19:51:05 +0530

    lets change the whole alogrithim

--- javascript-problems/longestCommonPrefixAnalysis.js
+++ javascript-problems/longestCommonPrefixAnalysis.js
@@ -7,9 +7,16 @@ const minLength = Math.min(str.map(s => s.length)) // the spread operator is rea
 // compare and give us minimum value because we use Math.min but i don t think this is right
 
 for (let i = 0; i < minLength; i++) { //yeah i found it will throw the error because the actual lenght of the arr is 2 only ok but we have 3 
-  if(str.length <= minLength) {
-    result += str[str.length - 1][i]
-  } // yeah i understand to only pass when the value is lesser then the minLength i think may less than or equal
+  
+  for(let j = 0; j < str.length; j++) {
+
+    if(i < str[j].length) {
+      result += str[j][i]
+    }
+
+  }
+
+  // yeah i understand to only pass when the value is lesser then the minLength i think may less than or equal
 } 
 
 console.log(result)
