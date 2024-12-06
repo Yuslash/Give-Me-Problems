ffdc94f2ed426ca9e658e903486a3eedf4795075
Yuslash <sibikrishna71@gmail.com>
2024-12-06 19:44:01 +0530

    what might be

--- javascript-problems/longestCommonPrefixAnalysis.js
+++ javascript-problems/longestCommonPrefixAnalysis.js
@@ -2,13 +2,14 @@ let result = ""
 
 let str = ['flower', 'flow']
 
-for (let i = 0; i < 2; i++) { //yeah i found it will throw the error because the actual lenght of the arr is 2 only ok but we have 3 
-  result += str[i][i]
-} 
 
 const minLength = Math.min(str.map(s => s.length)) // the spread operator is really good merging extracting like that so we are mapping then it will took 2 values and i think it will 
 // compare and give us minimum value because we use Math.min but i don t think this is right
 
+for (let i = 0; i < minLength; i++) { //yeah i found it will throw the error because the actual lenght of the arr is 2 only ok but we have 3 
+  if(str.length <= minLength) {
+    result += str[str.length - 1][i]
+  } // yeah i understand to only pass when the value is lesser then the minLength i think may less than or equal
+} 
 
-
-console.log(minLength)
+console.log(result)
