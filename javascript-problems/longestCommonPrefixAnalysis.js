14259565fe8586b23c50d14eaaa3547957e21663
Yuslash <sibikrishna71@gmail.com>
2024-12-06 19:58:43 +0530

    native way for that code

--- javascript-problems/longestCommonPrefixAnalysis.js
+++ javascript-problems/longestCommonPrefixAnalysis.js
@@ -2,21 +2,18 @@ let result = ""
 
 let str = ['flower', 'flow']
 
+//const minLength = Math.min(...str.map(s => s.length)) // to better understanding we can do that in native 
 
-const minLength = Math.min(str.map(s => s.length)) // the spread operator is really good merging extracting like that so we are mapping then it will took 2 values and i think it will 
-// compare and give us minimum value because we use Math.min but i don t think this is right
+let minLength = Infinity
 
-for (let i = 0; i < minLength; i++) { //yeah i found it will throw the error because the actual lenght of the arr is 2 only ok but we have 3 
-  
-  for(let j = 0; j < str.length; j++) {
+for(let i = 0; i < str.length; i++) {
 
-    if(i < str[j].length) {
-      result += str[j][i]
-    }
-
+  if(str[i].length < minLength) {
+    minLength = str[i].length
   }
 
-  // yeah i understand to only pass when the value is lesser then the minLength i think may less than or equal
-} 
+}
 
-console.log(result)
+//we got the least number of character from the array
+
+console.log(minLength)
