3a0dc2818552f7746afc1036a000c4c6f93edba2
Yuslash <sibikrishna71@gmail.com>
2024-12-06 22:59:00 +0530

    ok

--- simple-problems/minMax.js
+++ simple-problems/minMax.js
@@ -1,11 +1,18 @@
 var main = function(arr) {
 
+  let minimum = []
+  let maximum = []
+
   for(let i = 0; i < arr.length - 1; i++) {
+    
+    let currentIndex = 0
+    
     for(let j = i + 1; j < arr.length; j++) {
+
       if(arr[i] < arr[j]) {
+
         console.log(arr[i])
-      } else {
-        console.log("nothin just an inchident")
+
       }
          
     }
