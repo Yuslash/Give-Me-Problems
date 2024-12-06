b7f98b9f3c49cf8d4bd2b114893eed146427b76d
Yuslash <sibikrishna71@gmail.com>
2024-12-06 23:05:02 +0530

    but we have to find the only four minimum value so how

--- simple-problems/minMax.js
+++ simple-problems/minMax.js
@@ -3,20 +3,34 @@ var main = function(arr) {
   let minimum = []
   let maximum = []
 
+  let minResult = 0
+  let maxResult = 0
+
   for(let i = 0; i < arr.length - 1; i++) {
     
     let currentIndex = 0
     
     for(let j = i + 1; j < arr.length; j++) {
 
+// but only the four digits we need to take how?
+      
       if(arr[i] < arr[j]) {
 
-        console.log(arr[i])
-
+        minimum.push()
+        
       }
          
     }
   }
+
+  for(let i = 0; i < minimum.length; i++) {
+    let minResutlt += minimum[i]
+    let maxResult += maximum[i]
+  }
+
+  result = minResult + maxResult
+
+  return result 
 
 }
 
