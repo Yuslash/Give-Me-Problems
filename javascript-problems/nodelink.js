738408acc8900d32c1358c60377ac0b0e01b1173
Yuslash <sibikrishna71@gmail.com>
2024-12-10 15:04:57 +0530

    nothing

--- javascript-problems/nodelink.js
+++ javascript-problems/nodelink.js
@@ -1,17 +1,1 @@
-class Node {
-  constructor(data, pointer = null) {
-    this.data = data
-    this.pointer = pointer
-  }
-
-}
-
-let n1 = new Node(100)
-let n2 = new Node(200)
-let n3 = new Node(222)
-
-console.log(n1)
-console.log(n2)
-console.log(n3)
-
 
