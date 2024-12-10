14e02713a76a86363cf38831866bfc1a7f2d2076
Yuslash <sibikrishna71@gmail.com>
2024-12-11 00:43:24 +0530

    just done somthing

--- solving-arrays-problems/grading.js
+++ solving-arrays-problems/grading.js
@@ -1,23 +1,20 @@
-let ruleThree = 3
-let grades = [50, 61, 89, 87, 68, 33]
-
-console.log(grades[0] % 10)
+let grades = [50, 61, 89, 87, 68, 33, 73, 83]
 
 for (let i = 0; i < grades.length; i++) {
-  let lastValue = grades[i] % 10
-  let addingValue = grades[i] + 2
+  let grade = grades[i]
+  let lastValue = grade % 10
+  let addingValue = lastValue === 9 ? +1 : +2
 
   if (lastValue === 5 || lastValue === 0) {
     console.log("we can't round this up because this is already rounded up", grades[i])
   }
   else if (grades[i] < 38) {
     console.log("you are low mark mate sorry ")
-  }
-  else if (addingValue % 10 === 5 || addingValue % 10 === 0) {
-    console.log("your marks can be rouneded up", grades[i])
-  }
-  else {
-    console.log("wait let me do this wait")
+  } else if ((lastValue + addingValue) % 5 === 0) { // this is the formula grade is 68 + if lastdigit 9 no so +2 so it will be 70 rounded
+    // if the grade is 89 then 89 + 1 = 90 % 5 
+    console.log("Your marks can be rounded up to:", grade + addingValue)
+  } else {
+    console.log("this is values cannot be rounded because to rounded up it should minimum lesser than 3")
   }
 
 }
