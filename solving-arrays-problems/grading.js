71318e4f64306a407efc8be85076da321b2b6b1f
Yuslash <sibikrishna71@gmail.com>
2024-12-11 00:03:22 +0530

    yeah ok now that only lesser than 3 so 0 doesnt consider 1 or 2 maybe

--- solving-arrays-problems/grading.js
+++ solving-arrays-problems/grading.js
@@ -5,12 +5,16 @@ console.log(grades[0] % 10)
 
 for (let i = 0; i < grades.length; i++) {
   let lastValue = grades[i] % 10
+  let addingValue = grades[i] + 2
 
   if (lastValue === 5 || lastValue === 0) {
     console.log("we can't round this up because this is already rounded up", grades[i])
   }
   else if (grades[i] < 38) {
     console.log("you are low mark mate sorry ")
+  }
+  else if (addingValue % 10 === 5 || addingValue % 10 === 0) {
+    console.log("your marks can be rouneded up", grades[i])
   }
   else {
     console.log("wait let me do this wait")
