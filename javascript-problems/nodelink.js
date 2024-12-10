df155a02c2f8c577a7b4a8e557dce2cd784b9cc5
Yuslash <sibikrishna71@gmail.com>
2024-12-10 16:38:22 +0530

    nice

--- javascript-problems/nodelink.js
+++ javascript-problems/nodelink.js
@@ -1,1 +1,24 @@
+// now lets get into real business
+
+class Node {
+  constructor(data, next=null) {
+    this.data = data
+    this.head = next
+  }
+}
+
+const node = new Node(200, 390)
+
+console.log(node)
+
+
+const n1 = {
+  data: 200
+}
+
+const n2 = {
+  data: 300
+}
+
+n1.next = n2
 
