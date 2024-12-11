f0338e51556415eb8bed536cd7850bdb2bba431f
Yuslash <sibikrishna71@gmail.com>
2024-12-11 21:57:02 +0530

    why this isn't updated

--- solving-arrays-problems/grading.js
+++ solving-arrays-problems/grading.js
@@ -11,7 +11,7 @@ for (let i = 0; i < grades.length; i++) {
   else if (grades[i] < 38) {
     console.log("you are low mark mate sorry ")
   } else if ((lastValue + addingValue) % 5 === 0) { // this is the formula grade is 68 + if lastdigit 9 no so +2 so it will be 70 rounded
-    // if the grade is 89 then 89 + 1 = 90 % 5 
+    // if the grade is 89 then 89 + 1 = 90 % 5 will be 18 so remainder is 0 so if that condition true then it will add that grade + addingValue
     console.log("Your marks can be rounded up to:", grade + addingValue)
   } else {
     console.log("this is values cannot be rounded because to rounded up it should minimum lesser than 3")
