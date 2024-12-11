845b8dcf9ee1ea41f02cffce9b5a4a6435365151
Yuslash <sibikrishna71@gmail.com>
2024-12-11 22:25:48 +0530

    yeah done the timeconversion

---
+++ refresh/timeConversion.js
@@ -0,0 +1,19 @@
+function timeConversion(s) {
+  let period = s.slice(-2)
+  let hour = parseInt(s.slice(0, 2))
+  let rest = s.slice(2, -2)
+
+  if (period === "PM") {
+    if (hour !== 12) {
+      hour += 12
+    }
+  } else if (hour === 12) {
+    hour = 0
+  }
+
+  return `${hour.toString().padStart(2, '0')}${rest}`
+
+}
+
+console.log(timeConversion("07:05:45PM"))
+
