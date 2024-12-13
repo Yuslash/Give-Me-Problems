a2cb642834e44b7f20e70df4023be88a5baca862
Yuslash <sibikrishna71@gmail.com>
2024-12-13 14:20:06 +0530

    so yes as you can see we are getting correct value in javascript the problem is with like in math.floor or in the division

--- refresh/clumsyFactorial.py
+++ refresh/clumsyFactorial.py
@@ -1,3 +1,22 @@
 # in javascript we got the wrong value i think maybe the logic is not right but we are gonna do the same thing in python check are we getting the same value
 
+def main(n):
+    stack = [n]
+    n -= 1
+    index = 0
 
+    while n > 0:
+        if index % 4 == 0:
+            stack.append(stack.pop() * n)
+        elif index % 4 == 1:
+            stack.append(int(stack.pop() / n))
+        elif index % 4 == 2:
+            stack.append(n)
+        elif index % 4 == 3:
+            stack.append(-n)
+        n -= 1
+        index += 1
+
+    return sum(stack)
+
+print(main(10))
