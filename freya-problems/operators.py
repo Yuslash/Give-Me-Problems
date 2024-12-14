0c9b043528482e60c072a26e2f97731e75ba64ea
Yuslash <sibikrishna71@gmail.com>
2024-12-14 23:09:51 +0530

    yeah still learning this

--- freya-problems/operators.py
+++ freya-problems/operators.py
@@ -14,4 +14,11 @@
 # print(a | b)
 
 # this is XOR operator 
+# xor if the both are same example true, true = false and false, false = false 
+# then xor if both are different then it will return true true false = true and false true = true
+# a = 2
+# b = 3
+# print(a ^ b)
+
+# now left shift and rigt shift but i still dont understand that
 
