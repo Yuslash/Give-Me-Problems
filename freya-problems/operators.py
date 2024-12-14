0eb77cf2a21f60e10b0de6bb1d3f7ec37b2c2258
Yuslash <sibikrishna71@gmail.com>
2024-12-14 23:27:13 +0530

    this is actually good

--- freya-problems/operators.py
+++ freya-problems/operators.py
@@ -21,7 +21,9 @@
 # print(a ^ b)
 
 # now left shift and rigt shift but i still dont understand that
-num = 11
-binary_with_prefix = bin(num)
-binary_bits = bin(num)[2:]
-print(binary_bits)
+# num = 11
+# binary_with_prefix = bin(num)
+# binary_bits = bin(num)[2:]
+# print(binary_bitsa)
+
+# and we are gonna now get into the function 
