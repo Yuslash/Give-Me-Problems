356852c46d44caee3185da6bb84139abb684cafb
Yuslash <sibikrishna71@gmail.com>
2024-12-14 22:48:50 +0530

    i know

--- freya-problems/operators.py
+++ freya-problems/operators.py
@@ -1,5 +1,17 @@
-num = 10
-print(num > 5 and num < 15) # both needs to be true 
-print(num % 2 == 0 or num % 3 == 0) # if any one true then ok 
-print(not (num == 0)) # even if it false then it will be true
+# num = 10
+# print(num > 5 and num < 15) # both needs to be true 
+# print(num % 2 == 0 or num % 3 == 0) # if any one true then ok 
+# print(not (num == 0)) # even if it false then it will be true
+
+# ths is and operator
+# a = 2
+# b = 3
+# print(a & b)
+
+# this is or operator 
+# a = 2
+# b = 3
+# print(a | b)
+
+# this is XOR operator 
 
