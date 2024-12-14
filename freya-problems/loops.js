7b1881e415e06d6c5143b77f122b7c9736e864d5
Yuslash <sibikrishna71@gmail.com>
2024-12-15 00:00:00 +0530

    wait

---
+++ freya-problems/loops.js
@@ -0,0 +1,9 @@
+function main(n) {
+  for (let i = 0; i < n; i++) {
+    let result = i ** 2
+    console.log(result)
+  }
+}
+
+let n = 3
+main(n)
---
+++ freya-problems/loops.py
@@ -0,0 +1,7 @@
+def loops(n):
+    for i in range(n):
+        result = i**2
+        print(result)
+
+n = 3            
+loops(n)
