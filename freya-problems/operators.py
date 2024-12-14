488613cbc56574678e4dfd71f84c557c2d8eb130
Yuslash <sibikrishna71@gmail.com>
2024-12-14 20:42:52 +0530

    yeah good

---
+++ freya-problems/operators.py
@@ -0,0 +1,5 @@
+num = 10
+print(num > 5 and num < 15) # both needs to be true 
+print(num % 2 == 0 or num % 3 == 0) # if any one true then ok 
+print(not (num == 0)) # even if it false then it will be true
+
