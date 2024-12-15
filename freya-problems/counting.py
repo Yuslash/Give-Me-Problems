cd8b020cc053a02f2530c02abc67397227e5fe6c
Yuslash <sibikrishna71@gmail.com>
2024-12-15 20:55:47 +0530

    yeah i completed on own

--- freya-problems/counting.py
+++ freya-problems/counting.py
@@ -1,7 +1,11 @@
 def main(n):
     stack = []
+    countedStack = []
     for i in range(n + 1):
         stack.append(bin(i)[2:])
-    print(stack)
+    for j in range(len(stack)):
+       countedStack.append(bin(j)[2:].count('1'))
+    print(countedStack)
+
 n = 5
 main(n)
---
+++ freya-problems/practice.py
@@ -0,0 +1,3 @@
+for i in range(5 + 1):
+    result = bin(i)[2:].count('1')
+    print(result)
