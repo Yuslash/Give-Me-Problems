b191e6ac0c424471ee3e66e320a4f9f172745e35
Yuslash <sibikrishna71@gmail.com>
2024-12-19 00:46:21 +0530

    ada pongada

--- refresh/dp/numberlineJump.py
+++ refresh/dp/numberlineJump.py
@@ -1,14 +1,29 @@
+# x1 = 2
+# v1 = 1
+# final = x1 + v1
+#
+# x2 = 1
+# v2 = 2
+#
+# sefinal = x2 + v2
+#
+# if(x1 + v1 == final) and (x2 + v2 == sefinal):
+#     return("YES")
+# else:
+#     return("NO")
+
+def main(x1,x2,v1,v2):
+    if v1 == v2:
+       return "YES" if x1 == x2 else "NO"
+
+    if (x1 - x2) % (v1 - v2) == 0 and (x1 - x2) / (v1- v2) > 0:
+        return "YES" 
+
+    return "NO"
+
 x1 = 2
-v1 = 1
-
-final = x1 + v1
-
 x2 = 1
-v2 = 2
+v1 = 2
+v2 = 1
+print(main(x1, x2, v1, v2))
 
-sefinal = x2 + v2
-
-if(x1 + v1 == final) && (x2 + v2 == sefinal):
-    print("YES")
-else:
-    print("NO")
