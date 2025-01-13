f1d3e34f40e8d189faebfa0fa99817c6898bd06a
Yuslash <sibikrishna71@gmail.com>
2025-01-14 00:10:39 +0530

    wasted ms second life

--- formulas/windowSize.js
+++ formulas/windowSize.js
@@ -2,3 +2,4 @@ const screenWidth = window.innerWidth
 const screenHeight = window.innerHeight
 
 console.log(`Width: ${screenWidth}, Height: ${screenHeight}`)
+// how ar you da venna that is not the vscode that is something else 
